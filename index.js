6631133b6caec8d332a3cd2cd37e034888c72ce9
sowrav921 <sowravg987@gmail.com>
2023-11-08 22:39:23 +0600

    all methord compleate

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const foodCollection = client.db('foodDB').collection('foods');
     const  requestCollection = client.db('requestDB').collection('requests');
